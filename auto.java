66add33e6a9c58facf67d58f56e960fd22eba035
automation777 <31100666+automation777@users.noreply.github.com>
2017-08-17 18:56:14 +0530

    Add files via upload

---
+++ auto.java
@@ -0,0 +1,10 @@
+public class auto
+{
+	public static void main(String[] args)
+	{
+		for(int i=1 ; i <= 10 ; i++)
+		{
+			System.out.println("HELLO WORLD WELCOME TO NEW PROGRAMMING WORLD......." +i);
+		}
+	}
+}
